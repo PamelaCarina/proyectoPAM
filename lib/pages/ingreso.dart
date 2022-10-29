7291d40832a2a559396947d394d732dbd4127964
Carlos <cvenegasa@udec.cl>
2022-10-28 21:21:37 -0300

    commit final me voy a carretiar

--- lib/pages/ingreso.dart
+++ lib/pages/ingreso.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:democlase3/services/ingresoService.dart';
 import 'supermensajes.dart';
-
+import 'package:fluttertoast/fluttertoast.dart';
 
 class ingreso extends StatefulWidget {
   @override
@@ -9,14 +9,16 @@ class ingreso extends StatefulWidget {
 }
 
 class _ingresoState extends State<ingreso>{
+
   TextEditingController tituloController= TextEditingController();
   TextEditingController descripcionController=TextEditingController();
   @override
   Widget build(BuildContext context){
+    const sizedBox = const SizedBox(height: 30);
     return Scaffold(
       appBar: AppBar(
-          centerTitle: true,
-          title: Text('Titulo'),
+          backgroundColor: Colors.white,
+          title: Text('Titulo',style:TextStyle(color:Colors.black)),
       ),
       body: Container(
         padding: EdgeInsets.all(20),
@@ -25,8 +27,42 @@ class _ingresoState extends State<ingreso>{
           TextField(
             decoration: InputDecoration(
               border: OutlineInputBorder(), hintText: 'Ingrese titulo aqui'),
-
+            controller: tituloController,
           ),
+          TextField(
+            minLines: 15,
+            keyboardType: TextInputType.multiline,
+            maxLines: null,
+            decoration: InputDecoration(
+                border: OutlineInputBorder(), hintText: 'Ingrese texto aqui'),
+            controller: descripcionController,
+          ),
+          sizedBox,
+          sizedBox,
+          SizedBox(
+              width: double.infinity,
+              height: 30,
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.indigo, shape: StadiumBorder()),
+                  onPressed: () {
+                    if (tituloController.text.length == 0) {
+                      Fluttertoast.showToast(
+                          msg: "Ingrese un titulo valido",
+                          toastLength: Toast.LENGTH_SHORT,
+                          gravity: ToastGravity.CENTER,
+                          timeInSecForIosWeb: 1,
+                          backgroundColor: Colors.red,
+                          textColor: Colors.white,
+                          fontSize: 16.0);
+                    } else {
+                      IngresoService().validar(
+                          tituloController.text, descripcionController.text);
+                      Navigator.push(
+                          context, MaterialPageRoute(builder: (context) => supermensajes()));
+                    }
+                  },
+                  child: Text("Enviar"))),
         ],
         ),
       ),
@@ -34,12 +70,6 @@ class _ingresoState extends State<ingreso>{
 }
 
 
-  Future<void> validarMensaje(String titulo, String descripcion) async{
-    final response= await IngresoService().validar(titulo, descripcion);
-    if(response.statusCode==201){
-      Navigator.push(
-          context, MaterialPageRoute(builder: (context) => supermensajes()));
-    }
-  }
+
 
 }
--- lib/services/ingresoService.dart
+++ lib/services/ingresoService.dart
@@ -2,19 +2,29 @@ import 'dart:convert';
 import 'package:intl/intl.dart';
 import 'package:democlase3/global.dart';
 import 'package:http/http.dart' as http;
-
+import 'package:democlase3/postMensaje.dart';
 class IngresoService {
-  Future<http.Response> validar(String titulo, String descripcion) async {
-    return await http.post(
-      Uri.parse('https://40fd422c6d4d.sa.ngrok.io/api/Usuarios'),
+  Future<postMensaje> validar(String titulo, String descripcion) async {
+    final response = await http.post(
+      Uri.parse('https://40fd422c6d4d.sa.ngrok.io/api/mensajes'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
-      body: jsonEncode(<String, String>{'login': Global.login,
+      body: jsonEncode(<String, String>{
+        'login': Global.login,
         'fecha': DateFormat("yyyy-MM-dd hh:mm:ss").format(DateTime.now()),
         'titulo': titulo,
         'texto' : descripcion
       }),
     );
+    if (response.statusCode == 201) {
+      // If the server did return a 201 CREATED response,
+      // then parse the JSON.
+      return postMensaje.fromJson(jsonDecode(response.body));
+    } else {
+      // If the server did not return a 201 CREATED response,
+      // then throw an exception.
+      throw Exception('Failed to create album.');
+    }
   }
 }
